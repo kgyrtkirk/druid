3243cdd02a7b7e988143ef19c69d505296a043b0
Zoltan Haindrich <kirk@rxd.hu>
2024-03-12 17:18:28 +0000

    add

---
+++ sql/src/test/java/org/apache/druid/sql/avatica/DruidAvaticaHandlerTest3.java
@@ -0,0 +1,288 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.druid.sql.avatica;
+
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.inject.Injector;
+import com.google.inject.TypeLiteral;
+import com.google.inject.multibindings.Multibinder;
+import com.google.inject.name.Names;
+import org.apache.calcite.avatica.BuiltInConnectionProperty;
+import org.apache.calcite.avatica.server.AbstractAvaticaHandler;
+import org.apache.druid.common.config.NullHandling;
+import org.apache.druid.guice.LazySingleton;
+import org.apache.druid.guice.StartupInjectorBuilder;
+import org.apache.druid.initialization.CoreInjectorBuilder;
+import org.apache.druid.java.util.common.FileUtils;
+import org.apache.druid.java.util.common.StringUtils;
+import org.apache.druid.java.util.common.io.Closer;
+import org.apache.druid.java.util.emitter.service.ServiceEmitter;
+import org.apache.druid.math.expr.ExprMacroTable;
+import org.apache.druid.query.BaseQuery;
+import org.apache.druid.query.DefaultQueryConfig;
+import org.apache.druid.query.QueryRunnerFactoryConglomerate;
+import org.apache.druid.segment.join.JoinableFactoryWrapper;
+import org.apache.druid.server.DruidNode;
+import org.apache.druid.server.QueryLifecycleFactory;
+import org.apache.druid.server.QueryScheduler;
+import org.apache.druid.server.QuerySchedulerProvider;
+import org.apache.druid.server.QueryStackTests;
+import org.apache.druid.server.SpecificSegmentsQuerySegmentWalker;
+import org.apache.druid.server.log.RequestLogger;
+import org.apache.druid.server.log.TestRequestLogger;
+import org.apache.druid.server.metrics.NoopServiceEmitter;
+import org.apache.druid.server.security.AuthenticatorMapper;
+import org.apache.druid.server.security.AuthorizerMapper;
+import org.apache.druid.server.security.Escalator;
+import org.apache.druid.sql.calcite.planner.CalciteRulesManager;
+import org.apache.druid.sql.calcite.planner.CatalogResolver;
+import org.apache.druid.sql.calcite.planner.DruidOperatorTable;
+import org.apache.druid.sql.calcite.planner.PlannerConfig;
+import org.apache.druid.sql.calcite.schema.DruidSchemaCatalog;
+import org.apache.druid.sql.calcite.schema.DruidSchemaName;
+import org.apache.druid.sql.calcite.schema.NamedSchema;
+import org.apache.druid.sql.calcite.util.CalciteTests;
+import org.apache.druid.sql.guice.SqlModule;
+import org.eclipse.jetty.server.Server;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DruidAvaticaHandlerTest3
+    implements BeforeAllCallback, AfterAllCallback, BeforeEachCallback, AfterEachCallback
+{
+  private static final int CONNECTION_LIMIT = 4;
+  private static final int STATEMENT_LIMIT = 4;
+
+  private static final AvaticaServerConfig AVATICA_CONFIG;
+
+  static {
+    AVATICA_CONFIG = new AvaticaServerConfig();
+    // This must match the number of Connection objects created in
+    // testTooManyStatements()
+    AVATICA_CONFIG.maxConnections = CONNECTION_LIMIT;
+    AVATICA_CONFIG.maxStatementsPerConnection = STATEMENT_LIMIT;
+    System.setProperty("user.timezone", "UTC");
+  }
+
+  private static final String DUMMY_SQL_QUERY_ID = "dummy";
+
+  private static QueryRunnerFactoryConglomerate conglomerate;
+  private static SpecificSegmentsQuerySegmentWalker walker;
+  private static Closer resourceCloser;
+
+  private final boolean nullNumeric = !NullHandling.replaceWithDefault();
+
+  @Override
+  public void beforeAll(ExtensionContext context) throws Exception
+  {
+    // }
+    // @BeforeAll
+    // public static void setUpClass(@TempDir File tempDir)
+    // {
+    resourceCloser = Closer.create();
+    conglomerate = QueryStackTests.createQueryRunnerFactoryConglomerate(resourceCloser);
+    File tempDir = FileUtils.createTempDir("FIXME");
+    walker = CalciteTests.createMockWalker(conglomerate, tempDir);
+    resourceCloser.register(walker);
+  }
+
+  // @AfterAll
+  @Override
+  public void afterAll(ExtensionContext arg0) throws Exception
+  {
+    resourceCloser.close();
+  }
+
+  private final PlannerConfig plannerConfig = new PlannerConfig();
+  private final DruidOperatorTable operatorTable = CalciteTests.createOperatorTable();
+  private final ExprMacroTable macroTable = CalciteTests.createExprMacroTable();
+  private ServerWrapper server;
+  private Connection client;
+  private Connection clientNoTrailingSlash;
+  private Connection superuserClient;
+  private Connection clientLosAngeles;
+  private Connection clientLosAngelesUsingUrl;
+  private Injector injector;
+  private TestRequestLogger testRequestLogger;
+
+  private DruidSchemaCatalog makeRootSchema()
+  {
+    return CalciteTests.createMockRootSchema(
+        conglomerate,
+        walker,
+        plannerConfig,
+        CalciteTests.TEST_AUTHORIZER_MAPPER
+    );
+  }
+
+  private class ServerWrapper
+  {
+    final DruidMeta druidMeta;
+    final Server server;
+    final String url;
+
+    ServerWrapper(final DruidMeta druidMeta) throws Exception
+    {
+      this.druidMeta = druidMeta;
+      server = new Server(0);
+      server.setHandler(getAvaticaHandler(druidMeta));
+      server.start();
+      url = StringUtils.format(
+          "jdbc:avatica:remote:url=%s%s",
+          server.getURI().toString(),
+          StringUtils.maybeRemoveLeadingSlash(getJdbcUrlTail())
+      );
+    }
+
+    public Connection getConnection(String user, String password) throws SQLException
+    {
+      final Properties props = new Properties();
+      props.setProperty("user", user);
+      props.setProperty("password", password);
+      props.setProperty(BuiltInConnectionProperty.TRANSPARENT_RECONNECTION.camelName(), "true");
+      return DriverManager.getConnection(url, props);
+    }
+
+    public Connection getUserConnection() throws SQLException
+    {
+      return getConnection("regularUser", "druid");
+    }
+
+    // Note: though the URL-only form is OK in general, but it will cause tests
+    // to crash as the mock auth test code needs the user name.
+    // Use getUserConnection() instead, or create a URL that includes the
+    // user name and password.
+    // public Connection getConnection() throws SQLException
+    // {
+    // return DriverManager.getConnection(url);
+    // }
+
+    public void close() throws Exception
+    {
+      druidMeta.closeAllConnections();
+      server.stop();
+    }
+  }
+
+  protected String getJdbcUrlTail()
+  {
+    return DruidAvaticaJsonHandler.AVATICA_PATH;
+  }
+
+  // Default implementation is for JSON to allow debugging of tests.
+  protected AbstractAvaticaHandler getAvaticaHandler(final DruidMeta druidMeta)
+  {
+    return new DruidAvaticaJsonHandler(
+        druidMeta,
+        new DruidNode("dummy", "dummy", false, 1, null, true, false),
+        new AvaticaMonitor()
+    );
+  }
+
+  @Override
+  public void beforeEach(ExtensionContext arg0) throws Exception
+  {
+
+    final DruidSchemaCatalog rootSchema = makeRootSchema();
+    testRequestLogger = new TestRequestLogger();
+
+    injector = new CoreInjectorBuilder(new StartupInjectorBuilder().build())
+        .addModule(
+            binder -> {
+              binder.bindConstant().annotatedWith(Names.named("serviceName")).to("test");
+              binder.bindConstant().annotatedWith(Names.named("servicePort")).to(0);
+              binder.bindConstant().annotatedWith(Names.named("tlsServicePort")).to(-1);
+              binder.bind(AuthenticatorMapper.class).toInstance(CalciteTests.TEST_AUTHENTICATOR_MAPPER);
+              binder.bind(AuthorizerMapper.class).toInstance(CalciteTests.TEST_AUTHORIZER_MAPPER);
+              binder.bind(Escalator.class).toInstance(CalciteTests.TEST_AUTHENTICATOR_ESCALATOR);
+              binder.bind(RequestLogger.class).toInstance(testRequestLogger);
+              binder.bind(DruidSchemaCatalog.class).toInstance(rootSchema);
+              for (NamedSchema schema : rootSchema.getNamedSchemas().values()) {
+                Multibinder.newSetBinder(binder, NamedSchema.class).addBinding().toInstance(schema);
+              }
+              binder.bind(QueryLifecycleFactory.class)
+                  .toInstance(CalciteTests.createMockQueryLifecycleFactory(walker, conglomerate));
+              binder.bind(DruidOperatorTable.class).toInstance(operatorTable);
+              binder.bind(ExprMacroTable.class).toInstance(macroTable);
+              binder.bind(PlannerConfig.class).toInstance(plannerConfig);
+              binder.bind(String.class)
+                  .annotatedWith(DruidSchemaName.class)
+                  .toInstance(CalciteTests.DRUID_SCHEMA_NAME);
+              binder.bind(AvaticaServerConfig.class).toInstance(AVATICA_CONFIG);
+              binder.bind(ServiceEmitter.class).to(NoopServiceEmitter.class);
+              binder.bind(QuerySchedulerProvider.class).in(LazySingleton.class);
+              binder.bind(QueryScheduler.class)
+                  .toProvider(QuerySchedulerProvider.class)
+                  .in(LazySingleton.class);
+              binder.install(new SqlModule.SqlStatementFactoryModule());
+              binder.bind(new TypeLiteral<Supplier<DefaultQueryConfig>>()
+              {
+              }).toInstance(Suppliers.ofInstance(new DefaultQueryConfig(ImmutableMap.of())));
+              binder.bind(CalciteRulesManager.class).toInstance(new CalciteRulesManager(ImmutableSet.of()));
+              binder.bind(JoinableFactoryWrapper.class).toInstance(CalciteTests.createJoinableFactoryWrapper());
+              binder.bind(CatalogResolver.class).toInstance(CatalogResolver.NULL_RESOLVER);
+            }
+        )
+        .build();
+
+    DruidMeta druidMeta = injector.getInstance(DruidMeta.class);
+    server = new ServerWrapper(druidMeta);
+    client = server.getUserConnection();
+    superuserClient = server.getConnection(CalciteTests.TEST_SUPERUSER_NAME, "druid");
+    clientNoTrailingSlash = DriverManager.getConnection(
+        StringUtils.maybeRemoveTrailingSlash(server.url),
+        CalciteTests.TEST_SUPERUSER_NAME,
+        "druid"
+    );
+
+    final Properties propertiesLosAngeles = new Properties();
+    propertiesLosAngeles.setProperty("sqlTimeZone", "America/Los_Angeles");
+    propertiesLosAngeles.setProperty("user", "regularUserLA");
+    propertiesLosAngeles.setProperty(BaseQuery.SQL_QUERY_ID, DUMMY_SQL_QUERY_ID);
+    clientLosAngeles = DriverManager.getConnection(server.url, propertiesLosAngeles);
+  }
+
+  @Override
+  public void afterEach(ExtensionContext arg0) throws Exception
+  {
+    if (server != null) {
+      client.close();
+      clientLosAngeles.close();
+      clientNoTrailingSlash.close();
+      server.close();
+      client = null;
+      clientLosAngeles = null;
+      clientNoTrailingSlash = null;
+      server = null;
+    }
+  }
+}
