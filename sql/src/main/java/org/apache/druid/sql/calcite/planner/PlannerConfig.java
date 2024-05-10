3a1a2abf0847c35dc0f4bda5123559b169de99ca
Zoltan Haindrich <kirk@rxd.hu>
2024-05-10 12:48:37 +0000

    remove fixme

--- sql/src/main/java/org/apache/druid/sql/calcite/planner/PlannerConfig.java
+++ sql/src/main/java/org/apache/druid/sql/calcite/planner/PlannerConfig.java
@@ -433,7 +433,6 @@ public class PlannerConfig
 
   public Map<String, Object> getNonDefaultAsQueryContext()
   {
-    // FIXME finish this up!
     Map<String, Object> overrides = new HashMap<>();
     PlannerConfig def = new PlannerConfig();
     if (def.useApproximateCountDistinct != useApproximateCountDistinct) {
