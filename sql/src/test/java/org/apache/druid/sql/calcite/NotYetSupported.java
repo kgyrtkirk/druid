93dfc9bbdd2b70a4f94f63810e287ba59d7d6f8d
Zoltan Haindrich <kirk@rxd.hu>
2024-09-27 17:07:24 +0000

    fix issues/updates

--- sql/src/test/java/org/apache/druid/sql/calcite/NotYetSupported.java
+++ sql/src/test/java/org/apache/druid/sql/calcite/NotYetSupported.java
@@ -92,7 +92,6 @@ public @interface NotYetSupported
     UNSUPPORTED_DATASOURCE(DruidException.class, "WindowOperatorQuery must run on top of a query or inline data source"),
     UNION_WITH_COMPLEX_OPERAND(DruidException.class, "Only Table and Values are supported as inputs for Union"),
     UNION_MORE_STRICT_ROWTYPE_CHECK(DruidException.class, "Row signature mismatch in Union inputs"),
-    UNNEST_NOT_SUPPORTED_CORRELATE_CONVERSION(DruidException.class, "Missing conversion( is|s are) LogicalCorrelate"),
     SORT_REMOVE_TROUBLE(DruidException.class, "Calcite assertion violated.*Sort\\.<init>"),
     SORT_REMOVE_CONSTANT_KEYS_CONFLICT(DruidException.class, "not enough rules"),
     REQUIRE_TIME_CONDITION(CannotBuildQueryException.class, "requireTimeCondition is enabled"),
