3cc078a79de7ce57b5f8e76253f797a9009451bb
Zoltan Haindrich <kirk@rxd.hu>
2024-12-20 11:54:54 +0000

    refactor(BloomFilterSqlAggregatorTest.java): Refactored BloomFilterSqlAggregatorTest class to simplify schema configuration
    Simplified the schema configuration by removing unnecessary import statements and using a single schema builder.,Improved code readability by reorganizing the test class structure.

--- extensions-core/druid-bloom-filter/src/test/java/org/apache/druid/query/aggregation/bloom/sql/BloomFilterSqlAggregatorTest.java
+++ extensions-core/druid-bloom-filter/src/test/java/org/apache/druid/query/aggregation/bloom/sql/BloomFilterSqlAggregatorTest.java
@@ -29,8 +29,6 @@ import org.apache.druid.initialization.DruidModule;
 import org.apache.druid.java.util.common.granularity.Granularities;
 import org.apache.druid.query.Druids;
 import org.apache.druid.query.QueryRunnerFactoryConglomerate;
-import org.apache.druid.query.aggregation.CountAggregatorFactory;
-import org.apache.druid.query.aggregation.DoubleSumAggregatorFactory;
 import org.apache.druid.query.aggregation.FilteredAggregatorFactory;
 import org.apache.druid.query.aggregation.bloom.BloomFilterAggregatorFactory;
 import org.apache.druid.query.aggregation.bloom.sql.BloomFilterSqlAggregatorTest.BloomFilterSqlAggComponentSupplier;
@@ -44,7 +42,6 @@ import org.apache.druid.query.spec.MultipleIntervalSegmentSpec;
 import org.apache.druid.segment.IndexBuilder;
 import org.apache.druid.segment.QueryableIndex;
 import org.apache.druid.segment.column.ColumnType;
-import org.apache.druid.segment.incremental.IncrementalIndexSchema;
 import org.apache.druid.segment.join.JoinableFactoryWrapper;
 import org.apache.druid.segment.virtual.ExpressionVirtualColumn;
 import org.apache.druid.segment.writeout.OffHeapMemorySegmentWriteOutMediumFactory;
@@ -94,16 +91,7 @@ public class BloomFilterSqlAggregatorTest extends BaseCalciteQueryTest
           IndexBuilder.create()
                       .tmpDir(tempDirProducer.newTempFolder())
                       .segmentWriteOutMediumFactory(OffHeapMemorySegmentWriteOutMediumFactory.instance())
-                      .schema(
-                          new IncrementalIndexSchema.Builder()
-                              .withMetrics(
-                                  new CountAggregatorFactory("cnt"),
-                                  new DoubleSumAggregatorFactory("m1", "m1")
-                              )
-                              .withDimensionsSpec(TestDataBuilder.INDEX_SCHEMA_NUMERIC_DIMS.getDimensionsSpec())
-                              .withRollup(false)
-                              .build()
-                      )
+                      .schema(TestDataBuilder.INDEX_SCHEMA_NUMERIC_DIMS)
                       .rows(TestDataBuilder.ROWS1_WITH_NUMERIC_DIMS)
                       .buildMMappedIndex();
 
