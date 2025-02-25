c8078557d0a9fb27aacfb9d2289ac3c6d329c684
Zoltan Haindrich <kirk@rxd.hu>
2025-02-25 16:41:17 +0000

    extracted

--- extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/dart/controller/DartControllerContext.java
+++ extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/dart/controller/DartControllerContext.java
@@ -226,6 +226,12 @@ public class DartControllerContext implements ControllerContext
       final ControllerQueryKernelConfig queryKernelConfig
   )
   {
+    return extracted(queryKit, queryId, querySpec, queryKernelConfig);
+  }
+
+  public static QueryKitSpec extracted(final QueryKit<Query<?>> queryKit, final String queryId, final MSQSpec0 querySpec,
+      final ControllerQueryKernelConfig queryKernelConfig)
+  {
     final QueryContext queryContext = querySpec.getContext();
     return new QueryKitSpec(
         queryKit,
