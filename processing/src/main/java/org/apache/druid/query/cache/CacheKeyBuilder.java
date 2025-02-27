6e64b717a9e6dd5912998440ee9b2834016af1a9
Zoltan Haindrich <kirk@rxd.hu>
2025-02-27 15:08:26 +0000

    empty is valid

--- processing/src/main/java/org/apache/druid/query/cache/CacheKeyBuilder.java
+++ processing/src/main/java/org/apache/druid/query/cache/CacheKeyBuilder.java
@@ -20,7 +20,6 @@
 package org.apache.druid.query.cache;
 
 import com.google.common.base.Function;
-import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.primitives.Ints;
 import com.google.common.primitives.Longs;
@@ -32,7 +31,6 @@ import org.apache.druid.java.util.common.StringUtils;
 import javax.annotation.Nullable;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
@@ -108,7 +106,6 @@ public class CacheKeyBuilder
       return EMPTY_BYTES;
     } else {
       final byte[] key = cacheable.getCacheKey();
-      Preconditions.checkArgument(!Arrays.equals(key, EMPTY_BYTES), "cache key is equal to the empty key");
       return key;
     }
   }
