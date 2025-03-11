d52c8620e2032ba339b592098af2a8509237fcd7
Zoltan Haindrich <kirk@rxd.hu>
2025-03-11 11:03:51 +0000

    Revert "@Assited try"
    This reverts commit ce9a76c9ef0ce06b0c236275f372223294de6240.

--- extensions-core/multi-stage-query/src/test/java/org/apache/druid/msq/test/CalciteDartTest.java
+++ extensions-core/multi-stage-query/src/test/java/org/apache/druid/msq/test/CalciteDartTest.java
@@ -21,10 +21,7 @@ package org.apache.druid.msq.test;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.inject.Injector;
 import org.apache.druid.msq.dart.controller.sql.DartSqlEngine;
-import org.apache.druid.msq.test.DartComponentSupplier.I1;
-import org.apache.druid.msq.test.DartComponentSupplier.I1Factory;
 import org.apache.druid.sql.calcite.BaseCalciteQueryTest;
 import org.apache.druid.sql.calcite.QueryTestBuilder;
 import org.apache.druid.sql.calcite.SqlTestFrameworkConfig;
@@ -144,17 +141,4 @@ public class CalciteDartTest extends BaseCalciteQueryTest
         )
         .run();
   }
-
-
-  @Test
-  public void testComplexFromFoo1() throws Exception
-  {
-    Injector i = queryFrameworkRule.get().injector();
-    I1Factory instance = i.getInstance(I1Factory.class);
-    I1 e = instance.make(22);
-    e.i();
-
-  }
-
-
 }
