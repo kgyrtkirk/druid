573e8a0c1a7b495f5275d0fb150f669ade7c38cf
Zoltan Haindrich <kirk@rxd.hu>
2025-07-04 12:48:37 +0000

    correct prefix usage

--- extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/logical/stages/JoinStage.java
+++ extensions-core/multi-stage-query/src/main/java/org/apache/druid/msq/logical/stages/JoinStage.java
@@ -35,6 +35,7 @@ import org.apache.druid.segment.join.JoinConditionAnalysis;
 import org.apache.druid.segment.join.JoinType;
 import org.apache.druid.sql.calcite.expression.DruidExpression;
 import org.apache.druid.sql.calcite.expression.Expressions;
+import org.apache.druid.sql.calcite.planner.Calcites;
 import org.apache.druid.sql.calcite.planner.PlannerContext;
 import org.apache.druid.sql.calcite.planner.querygen.DruidQueryGenerator.DruidNodeStack;
 import org.apache.druid.sql.calcite.rel.DruidJoinQueryRel;
@@ -92,7 +93,8 @@ public class JoinStage
     private JoinConditionAnalysis conditionAnalysis;
     private JoinType joinType;
 
-    public SortMergeStage(RowSignature signature, List<LogicalInputSpec> inputs, String rightPrefix, JoinConditionAnalysis conditionAnalysis, JoinType joinType)
+    public SortMergeStage(RowSignature signature, List<LogicalInputSpec> inputs, String rightPrefix,
+        JoinConditionAnalysis conditionAnalysis, JoinType joinType)
     {
       super(signature, inputs);
       this.rightPrefix = rightPrefix;
@@ -118,117 +120,128 @@ public class JoinStage
     }
   }
 
-
-  /**  similar to {@link DruidJoinQueryRel#buildJoinSourceDesc}*/
+  /** similar to {@link DruidJoinQueryRel#buildJoinSourceDesc} */
   public static LogicalStage buildJoinStage(List<LogicalStage> inputStages, DruidNodeStack stack)
   {
     DruidJoin join = (DruidJoin) stack.getNode();
 
     return buildMergeJoin(inputStages, stack, join);
 
-//////      final QueryDefinitionBuilder subQueryDefBuilder = QueryDefinition.builder(queryKitSpec.getQueryId());
-////
-////      // Plan the left input.
-////      // We're confident that we can cast dataSource.getLeft() to QueryDataSource, because DruidJoinQueryRel creates
-////      // subqueries when the join algorithm is sortMerge.
-////      final DataSourcePlan leftPlan = forQuery(
-////          queryKitSpec,
-////          (QueryDataSource) dataSource.getLeft(),
-////          Math.max(minStageNumber, subQueryDefBuilder.getNextStageNumber()),
-////          false
-////      );
-////      leftPlan.getSubQueryDefBuilder().ifPresent(subQueryDefBuilder::addAll);
-////
-////      // Plan the right input.
-////      // We're confident that we can cast dataSource.getRight() to QueryDataSource, because DruidJoinQueryRel creates
-////      // subqueries when the join algorithm is sortMerge.
-////      final DataSourcePlan rightPlan = forQuery(
-////          queryKitSpec,
-////          (QueryDataSource) dataSource.getRight(),
-////          Math.max(minStageNumber, subQueryDefBuilder.getNextStageNumber()),
-////          false
-////      );
-////      rightPlan.getSubQueryDefBuilder().ifPresent(subQueryDefBuilder::addAll);
-////
-////
-////
-////      // Build up the left stage.
-////      final StageDefinitionBuilder leftBuilder = subQueryDefBuilder.getStageBuilder(
-////          ((StageInputSpec) Iterables.getOnlyElement(leftPlan.getInputSpecs())).getStageNumber()
-////      );
-//QueryKitSpec queryKitSpec;
-//      final int hashPartitionCount = queryKitSpec.getNumPartitionsForShuffle();
-//      final List<KeyColumn> leftPartitionKey = partitionKeys.get(0);
-//      leftBuilder.shuffleSpec(new HashShuffleSpec(new ClusterBy(leftPartitionKey, 0), hashPartitionCount));
-//      leftBuilder.signature(QueryKitUtils.sortableSignature(leftBuilder.getSignature(), leftPartitionKey));
-//
-//      // Build up the right stage.
-//      final StageDefinitionBuilder rightBuilder = subQueryDefBuilder.getStageBuilder(
-//          ((StageInputSpec) Iterables.getOnlyElement(rightPlan.getInputSpecs())).getStageNumber()
-//      );
-//
-//      final List<KeyColumn> rightPartitionKey = partitionKeys.get(1);
-//      rightBuilder.shuffleSpec(new HashShuffleSpec(new ClusterBy(rightPartitionKey, 0), hashPartitionCount));
-//      rightBuilder.signature(QueryKitUtils.sortableSignature(rightBuilder.getSignature(), rightPartitionKey));
-//
-//      // Compute join signature.
-//      final RowSignature.Builder joinSignatureBuilder = RowSignature.builder();
-//
-//      for (String leftColumn : leftBuilder.getSignature().getColumnNames()) {
-//        joinSignatureBuilder.add(leftColumn, leftBuilder.getSignature().getColumnType(leftColumn).orElse(null));
-//      }
-//
-//      for (String rightColumn : rightBuilder.getSignature().getColumnNames()) {
-//        joinSignatureBuilder.add(
-//            dataSource.getRightPrefix() + rightColumn,
-//            rightBuilder.getSignature().getColumnType(rightColumn).orElse(null)
-//        );
-//      }
-//
-//      // Build up the join stage.
-//      final int stageNumber = Math.max(minStageNumber, subQueryDefBuilder.getNextStageNumber());
-//
-//      subQueryDefBuilder.add(
-//          StageDefinition.builder(stageNumber)
-//                         .inputs(
-//                             ImmutableList.of(
-//                                 Iterables.getOnlyElement(leftPlan.getInputSpecs()),
-//                                 Iterables.getOnlyElement(rightPlan.getInputSpecs())
-//                             )
-//                         )
-//                         .maxWorkerCount(queryKitSpec.getMaxNonLeafWorkerCount())
-//                         .signature(joinSignatureBuilder.build())
-//                         .processor(
-//                             new SortMergeJoinStageProcessor(
-//                                 dataSource.getRightPrefix(),
-//                                 dataSource.getConditionAnalysis(),
-//                                 dataSource.getJoinType()
-//                             )
-//                         )
-//      );
-//
-//      return new DataSourcePlan(
-//          new InputNumberDataSource(0),
-//          Collections.singletonList(new StageInputSpec(stageNumber)),
-//          broadcast ? IntOpenHashSet.of(0) : IntSets.emptySet(),
-//          subQueryDefBuilder
-//      );
-//      }
-//    }    return null;
+    ////// final QueryDefinitionBuilder subQueryDefBuilder =
+    ////// QueryDefinition.builder(queryKitSpec.getQueryId());
+    ////
+    //// // Plan the left input.
+    //// // We're confident that we can cast dataSource.getLeft() to
+    ////// QueryDataSource, because DruidJoinQueryRel creates
+    //// // subqueries when the join algorithm is sortMerge.
+    //// final DataSourcePlan leftPlan = forQuery(
+    //// queryKitSpec,
+    //// (QueryDataSource) dataSource.getLeft(),
+    //// Math.max(minStageNumber, subQueryDefBuilder.getNextStageNumber()),
+    //// false
+    //// );
+    //// leftPlan.getSubQueryDefBuilder().ifPresent(subQueryDefBuilder::addAll);
+    ////
+    //// // Plan the right input.
+    //// // We're confident that we can cast dataSource.getRight() to
+    ////// QueryDataSource, because DruidJoinQueryRel creates
+    //// // subqueries when the join algorithm is sortMerge.
+    //// final DataSourcePlan rightPlan = forQuery(
+    //// queryKitSpec,
+    //// (QueryDataSource) dataSource.getRight(),
+    //// Math.max(minStageNumber, subQueryDefBuilder.getNextStageNumber()),
+    //// false
+    //// );
+    //// rightPlan.getSubQueryDefBuilder().ifPresent(subQueryDefBuilder::addAll);
+    ////
+    ////
+    ////
+    //// // Build up the left stage.
+    //// final StageDefinitionBuilder leftBuilder =
+    ////// subQueryDefBuilder.getStageBuilder(
+    //// ((StageInputSpec)
+    ////// Iterables.getOnlyElement(leftPlan.getInputSpecs())).getStageNumber()
+    //// );
+    // QueryKitSpec queryKitSpec;
+    // final int hashPartitionCount = queryKitSpec.getNumPartitionsForShuffle();
+    // final List<KeyColumn> leftPartitionKey = partitionKeys.get(0);
+    // leftBuilder.shuffleSpec(new HashShuffleSpec(new
+    ////// ClusterBy(leftPartitionKey, 0), hashPartitionCount));
+    // leftBuilder.signature(QueryKitUtils.sortableSignature(leftBuilder.getSignature(),
+    ////// leftPartitionKey));
+    //
+    // // Build up the right stage.
+    // final StageDefinitionBuilder rightBuilder =
+    ////// subQueryDefBuilder.getStageBuilder(
+    // ((StageInputSpec)
+    ////// Iterables.getOnlyElement(rightPlan.getInputSpecs())).getStageNumber()
+    // );
+    //
+    // final List<KeyColumn> rightPartitionKey = partitionKeys.get(1);
+    // rightBuilder.shuffleSpec(new HashShuffleSpec(new
+    ////// ClusterBy(rightPartitionKey, 0), hashPartitionCount));
+    // rightBuilder.signature(QueryKitUtils.sortableSignature(rightBuilder.getSignature(),
+    ////// rightPartitionKey));
+    //
+    // // Compute join signature.
+    // final RowSignature.Builder joinSignatureBuilder = RowSignature.builder();
+    //
+    // for (String leftColumn : leftBuilder.getSignature().getColumnNames()) {
+    // joinSignatureBuilder.add(leftColumn,
+    ////// leftBuilder.getSignature().getColumnType(leftColumn).orElse(null));
+    // }
+    //
+    // for (String rightColumn : rightBuilder.getSignature().getColumnNames()) {
+    // joinSignatureBuilder.add(
+    // dataSource.getRightPrefix() + rightColumn,
+    // rightBuilder.getSignature().getColumnType(rightColumn).orElse(null)
+    // );
+    // }
+    //
+    // // Build up the join stage.
+    // final int stageNumber = Math.max(minStageNumber,
+    ////// subQueryDefBuilder.getNextStageNumber());
+    //
+    // subQueryDefBuilder.add(
+    // StageDefinition.builder(stageNumber)
+    // .inputs(
+    // ImmutableList.of(
+    // Iterables.getOnlyElement(leftPlan.getInputSpecs()),
+    // Iterables.getOnlyElement(rightPlan.getInputSpecs())
+    // )
+    // )
+    // .maxWorkerCount(queryKitSpec.getMaxNonLeafWorkerCount())
+    // .signature(joinSignatureBuilder.build())
+    // .processor(
+    // new SortMergeJoinStageProcessor(
+    // dataSource.getRightPrefix(),
+    // dataSource.getConditionAnalysis(),
+    // dataSource.getJoinType()
+    // )
+    // )
+    // );
+    //
+    // return new DataSourcePlan(
+    // new InputNumberDataSource(0),
+    // Collections.singletonList(new StageInputSpec(stageNumber)),
+    // broadcast ? IntOpenHashSet.of(0) : IntSets.emptySet(),
+    // subQueryDefBuilder
+    // );
+    // }
+    // } return null;
 
   }
 
+  static int idx = 0;
 
   private static LogicalStage buildMergeJoin(List<LogicalStage> inputStages, DruidNodeStack stack, DruidJoin join)
   {
-    String prefix = "_j"; // FIXME
+    String prefix = findUnusedJoinPrefix(inputStages.get(0).getRowSignature());
 
     RowSignature signature = RowSignature.builder()
-      .addAll(inputStages.get(0).getLogicalRowSignature())
-      .addAll(inputStages.get(1).getLogicalRowSignature().withPrefix(prefix))
-      .build();
-
-
+        .addAll(inputStages.get(0).getLogicalRowSignature())
+        .addAll(inputStages.get(1).getLogicalRowSignature().withPrefix(prefix))
+        .build();
 
     PlannerContext plannerContext = stack.getPlannerContext();
     VirtualColumnRegistry virtualColumnRegistry = VirtualColumnRegistry.create(
@@ -245,11 +258,13 @@ public class JoinStage
         join.getCondition()
     );
 
-    // Unsetting it to avoid any VC Registry leaks incase there are multiple druid quries for the SQL
-    // It should be fixed soon with changes in interface for SqlOperatorConversion and Expressions bridge class
+    // Unsetting it to avoid any VC Registry leaks incase there are multiple
+    // druid quries for the SQL
+    // It should be fixed soon with changes in interface for
+    // SqlOperatorConversion and Expressions bridge class
     plannerContext.setJoinExpressionVirtualColumnRegistry(null);
 
-    if(!virtualColumnRegistry.isEmpty() ) {
+    if (!virtualColumnRegistry.isEmpty()) {
       throw DruidException.defensive("Not sure how to handle this right now - it should be fixed");
     }
 
@@ -257,32 +272,36 @@ public class JoinStage
         condition.getExpression(),
         plannerContext.parseExpression(condition.getExpression()),
         prefix
-        );
+    );
 
-    //FIXME?      checkQuerySegmentSpecIsEternity(dataSource, querySegmentSpec);
-    //   FIXME   SortMergeJoinStageProcessor.validateCondition(join.getCondition());
+    // FIXME? checkQuerySegmentSpecIsEternity(dataSource, querySegmentSpec);
+    // FIXME SortMergeJoinStageProcessor.validateCondition(join.getCondition());
 
-      // Partition by keys given by the join condition.
-      final List<List<KeyColumn>> partitionKeys = SortMergeJoinStageProcessor.toKeyColumns(
-          SortMergeJoinStageProcessor.validateCondition(analysis)
-      );
+    // Partition by keys given by the join condition.
+    final List<List<KeyColumn>> partitionKeys = SortMergeJoinStageProcessor.toKeyColumns(
+        SortMergeJoinStageProcessor.validateCondition(analysis)
+    );
 
+    List<LogicalStage> shuffleStages = new ArrayList<>();
+    for (int i = 0; i < inputStages.size(); i++) {
+      LogicalStage inputStage = inputStages.get(i);
+      shuffleStages.add(new ShuffleStage(inputStage, partitionKeys.get(i)));
 
+    }
 
-      List<LogicalStage> shuffleStages = new ArrayList<>();
-      for (int i = 0; i < inputStages.size(); i++) {
-        LogicalStage inputStage = inputStages.get(i);
-        shuffleStages.add(new ShuffleStage(inputStage, partitionKeys.get(i)));
+    // shuffleStages.get(0)
 
-      }
+    return new SortMergeStage(
+        signature, Lists.transform(shuffleStages, LogicalInputSpec::of),
+        prefix,
+        analysis,
+        DruidJoinQueryRel.toDruidJoinType(join.getJoinType())
+    );
+  }
 
-
-//      shuffleStages.get(0)
-
-      return new SortMergeStage(signature, Lists.transform(shuffleStages, LogicalInputSpec::of),
-          prefix,
-          analysis,
-          DruidJoinQueryRel.toDruidJoinType(join.getJoinType())
-          );
+  private static String findUnusedJoinPrefix(RowSignature rowSignature)
+  {
+    List<String> leftColumnNames = rowSignature.getColumnNames();
+    return Calcites.findUnusedPrefixForDigits("j", leftColumnNames) + "0";
   }
 }
